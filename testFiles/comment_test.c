f927ae914298997114095ff463b628e1264b2512
Somnath Sarkar <somnath.k.sarkar@gmail.com>
2018-01-10 10:32:45 +0000

    Create comment_test.c

---
+++ testFiles/comment_test.c
@@ -0,0 +1,12 @@
+
+// This is a valid single line comment
+
+/t/ This is an invalid single line comment  
+
+/* 
+	This is a valid multi-line comment
+*/
+
+/*
+	This is an invalid multiline comment
+//
