2bec697d9b538504651ab6e6ff8a46e9c10aa5cf
Menezes Elvis Edward <menezeselvis7@gmail.com>
2018-01-10 09:49:24 +0000

    Create dataType_test.c

---
+++ testFiles/dataType_test.c
@@ -0,0 +1,39 @@
+/*
+	This is a program to test the compiler for handling basic datatypes, comments and basic arithmetic operations
+*/
+
+int main(){
+	//Int dataType declarations
+	int a;
+	int b;
+	int c;
+	int k;
+		
+	// Correct test case for int dataType
+	a = 10;
+	
+	//Incorrect test case for float dataType
+	k = 2x;
+	
+	//Arithmetic operations on int dataType
+	int c;
+	c = a + b;
+	
+	//float dataType declarations
+	float e;
+	float f;
+	float g;
+	
+	//Correct test case for float dataTypes	
+	e = 1.00;
+	f = 2.00;
+	g = 3;
+	float g = f - e;
+	
+	//TestCases for checking the valid variable conventions
+	
+	int abc;
+	int abc_9;
+	//Invalid Case:
+	int 9_abc;
+}
