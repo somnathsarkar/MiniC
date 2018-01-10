5f99eaba6dac128f179b02476f164b4889fdf983
Somnath Sarkar <somnath.k.sarkar@gmail.com>
2018-01-10 16:24:44 +0530

    Update functionCall_test.c

--- testFiles/functionCall_test.c
+++ testFiles/functionCall_test.c
@@ -15,15 +15,14 @@ int main(){
 	int i, j = 3;
 	float abc_9;
 	abc_9 = 10.00;
+	//Valid function calls
 	printf("Hello World\n");
 	if( i < 2)
 		functionCall(abc_9);
 	else 
 		j = 3;
-	// Invalid number of parameters to call the function functionCall in the if condition
-	if( j > 1)
-		functionCall();					
-	else
-		functionCall(abc_9);
+	//Invalid function calls
+	functionCall;
+	functionCall[];
 }
 		
