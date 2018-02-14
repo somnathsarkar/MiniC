2cca1de331f072a691a69ff277e273ec0dc62c06
Somnath Sarkar <somnath.k.sarkar@gmail.com>
2018-02-14 16:14:35 +0530

    Updated symbol table

--- Project2/symboltable.c
+++ Project2/symboltable.c
@@ -1,8 +1,9 @@
 #include "symboltable.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-extern int yylineno;
+extern int lineno;
 
 struct symboltable *createsymboltable(){
     struct symboltable *ret = (struct symboltable *)malloc(sizeof(struct symboltable));
@@ -12,14 +13,14 @@ struct symboltable *createsymboltable(){
 
 void add_identifier(struct symboltable *st, char *id, char *dtype){
     if(lookup(st,id)){
-        printf("Warning: Duplicate Definition of %s in this scope on Line %d\n",id,yylineno);
+        printf("Warning: Duplicate Definition of %s in this scope on Line %d\n",id,lineno);
         return;
     }
     if((st->root)==NULL){
         st->root = (struct node *)malloc(sizeof(struct node));
         (st->root)->id = strdup(id);
         (st->root)->dtype = strdup(dtype);
-        (st->root)->line = yylineno;
+        (st->root)->line = lineno;
         (st->root)->next = NULL;
     }else{
         struct node* focus = st->root;
@@ -27,7 +28,7 @@ void add_identifier(struct symboltable *st, char *id, char *dtype){
         focus->next = (struct node *)malloc(sizeof(struct node));
         (focus->next)->id = strdup(id);
         (focus->next)->dtype = strdup(dtype);
-        (focus->next)->line = yylineno;
+        (focus->next)->line = lineno;
         (focus->next)->next = NULL;
     }
 }
